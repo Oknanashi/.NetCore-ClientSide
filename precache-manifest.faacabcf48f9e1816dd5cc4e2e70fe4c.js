d3f3c4d1defb1da2e45c5974d345a0c0400e0485
Oknanashi <oknanashi@gmail.com>
2020-03-13 21:39:57 -0400

    Updates

---
+++ precache-manifest.faacabcf48f9e1816dd5cc4e2e70fe4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322293b3b6de02268b74fa1b9f371bb2",
+    "url": "/netcore-clientside/index.html"
+  },
+  {
+    "revision": "5864f25d6f89d95bd6b3",
+    "url": "/netcore-clientside/static/css/2.49293bbc.chunk.css"
+  },
+  {
+    "revision": "337a1a73d34751e7ba18",
+    "url": "/netcore-clientside/static/css/main.dcfde02c.chunk.css"
+  },
+  {
+    "revision": "5864f25d6f89d95bd6b3",
+    "url": "/netcore-clientside/static/js/2.d8333f0a.chunk.js"
+  },
+  {
+    "revision": "2f0ee2faf55c5eb9ea9267046a5519af",
+    "url": "/netcore-clientside/static/js/2.d8333f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337a1a73d34751e7ba18",
+    "url": "/netcore-clientside/static/js/main.3feec99f.chunk.js"
+  },
+  {
+    "revision": "24e3bc62370366f57231",
+    "url": "/netcore-clientside/static/js/runtime-main.3b1e3eea.js"
+  }
+]);
